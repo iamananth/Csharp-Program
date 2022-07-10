65f89c5526abbe04206c57bc72ae148eba37584b
iamananth <ananthrshenoy@gmail.com>
2022-07-10 17:22:09 +0530

    first and second largest

---
+++ ConsoleApp8/first_and_second_large.cs
@@ -0,0 +1,35 @@
+﻿namespace ConsoleApp8
+{
+    internal class first_and_second_large
+    {
+        public static void Main1(string[] args)
+        {
+            int max, secMax;
+
+            Console.WriteLine("Enter the no of elements :");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = new int[n];
+
+            Console.WriteLine("Enter the elements : ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            max = secMax = arr[0];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > max)
+                {
+                    secMax = max;
+                    max = arr[i];
+                }
+                else if (arr[i] > secMax && arr[i] < max)
+                {
+                    secMax = arr[i];
+                }
+            }
+            Console.WriteLine("The largest Element is " + max + " and the second largest element is " + secMax);
+        }
+    }
+}
